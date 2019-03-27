52ab37f98505a92d6e33d3f717ec848666223898
echemendiac <cechemendia@my.apsu.edu>
2019-03-26 20:22:17 -0400

    Initial commit

---
+++ app/src/main/java/com/example/chris/final_project_csci_4020/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.chris.final_project_csci_4020;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
