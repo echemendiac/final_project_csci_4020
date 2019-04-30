0597491bb07d442b9a92714c342f133b80804cae
dsmith181 <dsmith181@my.apsu.edu>
2019-04-29 23:38:29 -0400

    Got the spinners working and created some object language for them to follow

--- app/src/main/java/com/example/chris/final_project_csci_4020/CreateEstimate.java
+++ app/src/main/java/com/example/chris/final_project_csci_4020/CreateEstimate.java
@@ -7,35 +7,55 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class CreateEstimate extends AppCompatActivity {
-    Dropdown    roofType_dd,
-                pipeFlahsingColor_dd,
-                sidingType_dd,
-                sidingColor_dd,
-                gutterSize_dd,
-                gutterColor_dd,
-                downspoutSize_dd;
+    Spinner     roofType_s,
+                roofColor_s,
+                pipeFlahsingColor_s,
+                sidingType_s,
+                sidingColor_s,
+                gutterSize_s,
+                gutterColor_s,
+                downspoutSize_s;
 
-    Context context;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_estimate);
-        context = CreateEstimate.this;
-                roofType_dd = new Dropdown(context,)
-                pipeFlahsingColor_dd,
-                sidingType_dd,
-                sidingColor_dd,
-                gutterSize_dd,
-                gutterColor_dd,
-                downspoutSize_dd
+
+        //Spinneers act like drop downs
+
+        //---- Setting Up Spinners ----//
+        setupSpinner(roofType_s, R.id.roofType_s, R.array.roofType);
+        setupSpinner(roofColor_s,R.id.roofColor_s,R.array.shingleRoofColors);
+       // setupSpinner(roofColor_s,R.id.roofColor_s,R.array.shingleRoofColors);
 
 
 
+//                pipeFlahsingColor_s = new
+//                sidingType_s,
+//                sidingColor_s,
+//                gutterSize_s,
+//                gutterColor_s,
+//                downspoutSize_s
 
+
+    }
+    private void setupSpinner(Spinner spinner, int viewId, int arrayId){
+        final Context context = CreateEstimate.this;
+        //Setup Spinner
+        spinner = findViewById(viewId);
+        ArrayAdapter<String> spinner_aa = new ArrayAdapter<String>(
+                context,
+                R.layout.list_item ,
+                getResources().getStringArray(arrayId));
+        spinner_aa.setDropDownViewResource(R.layout.dropdown_item);
+        spinner.setAdapter(spinner_aa);
     }
 
 }
